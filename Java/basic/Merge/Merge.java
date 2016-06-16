17d6663994aaf8564f613e16c2ef5c3ab1973367
nanijiayou <heyuanjiayou@gmail.com>
2016-06-16 15:40:29 +0800

    归并

--- Java/basic/Merge/Merge.java
+++ Java/basic/Merge/Merge.java
@@ -6,30 +6,30 @@ public class Merge {
 
 	private Merge() {}
 
+	/*归并算法核心实现*/
+	/*合并*/
 	private static void merge(Comparable[] a, Comparable[] aux, int lo, int mid, int hi) {
 		assert isSorted(a, lo, mid);
 		assert isSorted(a, mid+1, hi);
-
-		for(var int k = lo; k <= hi; k++)
+		
+		for(int k = lo, k <= hi; k++) {
 			aux[k] = a[k];
-
-		int i = lo, j = mid + 1;
-
-		for(int k = lo; k < hi; k++) {
-			if(i > mid) a[k] = aux[j++];
-			if(j > hi)  a[k] = aux[i++];
-			else if(less(a[i], a[j])) a[k] = a[i++];
-			else a[k] = a[j++];
 		}
 
-		assert isSorted(a, lo, hi);
+		int i = lo; j = mid+1;
+		for(int k = lo; k <= hi; k++){
+			if(i > mid) a[k] = aux[j++];
+			if(j > hi)  a[k] = aux[i++];
+			else if(less(a[i], a[j])) a[k] = [i++];
+			else a[k] = aux[j++];
+		}
 	}
-
+	/*递归分解*/
 	private static void sort(Comparable[] a, Comparable[], aux, int lo, int hi) {
 		if(hi <= lo) return;
-		int mid = lo + (hi - mid) / 2;
+		int mid = lo + (hi - ho) / 2;
 		sort(a, aux, lo, mid);
-		sort(a, aux, mid + 1, hi);
+		sort(a, aux, mid+1, hi);
 		merge(a, aux, lo, mid, hi);
 	}
 
@@ -38,4 +38,30 @@ public class Merge {
 		sort(a, aux, 0, a.length - 1);
 		assert isSorted(a);
 	}
+
+
+	/*helper functions*/
+	private static boolean less(Comparable v, Comparable w) {
+		return v.compareTo(w) < 0;
+	}
+	private static void exch(Object[] a, int i, int j) {
+		Object temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+	}
+	private static boolean isSorted(Comparable[] a) {
+		return isSorted(a, 0, a.length - 1);
+	}
+	private static boolean isSorted(Comparable[], int lo, int hi) {
+		for(int k = lo; k <= hi; k++)		
+			if(less(a[k], a[k-1])) return false;
+		return true;
+	}
+	private static void show(Comparable[] a) {
+		for(int i = 0; i < a.lenght; i++)
+			StdOut.println(a[i]);
+	} 
+
+	/*main函数*/
+
 }
